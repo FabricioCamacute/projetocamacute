d66894bb30764664f9c1dc3ff59f3ae5ac2fea33
FabricioCamacute <166270109+FabricioCamacute@users.noreply.github.com>
2024-04-11 09:16:06 -0300

    Alteração 7

---
+++ Hello World/main.js
@@ -0,0 +1,14 @@
+alert("Olá mundo ! Docente Fabricio Camaçute parabenizando a equipe da Alura. Excelentes vídeos publicados.");
+let nomeUsuário = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual é seu nome?");
+
+}
+if{nomeUsuário ==}{
+    elemento.textContent = "Seja muito bem-vindo";
+} else{
+    elemento.textContent
+}
+elemento.textContent = nomeUsuário;
